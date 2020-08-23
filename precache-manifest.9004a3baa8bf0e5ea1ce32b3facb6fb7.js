70e91929dbde3a72bf1d83740dd39909de788bae
Max <maxim.sagan@sympli.com.au>
2020-08-24 00:59:20 +1000

    Updates

---
+++ precache-manifest.9004a3baa8bf0e5ea1ce32b3facb6fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93a0a6501bb580e3041ec685d4ca7e5",
+    "url": "/maximum-memory/index.html"
+  },
+  {
+    "revision": "15bb957234b6db8a28bd",
+    "url": "/maximum-memory/static/css/main.6c986bf0.chunk.css"
+  },
+  {
+    "revision": "dd7570ce6847f4a39aa9",
+    "url": "/maximum-memory/static/js/2.4d9b5e64.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/maximum-memory/static/js/2.4d9b5e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bb957234b6db8a28bd",
+    "url": "/maximum-memory/static/js/main.743c192b.chunk.js"
+  },
+  {
+    "revision": "ab632f5b80289e35588b",
+    "url": "/maximum-memory/static/js/runtime-main.fb30ab0a.js"
+  }
+]);
